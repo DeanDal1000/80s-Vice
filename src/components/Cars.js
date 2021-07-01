9c0dbe0e44285ca6f90a22d78b3b886a3077fead
DeanDal1000 <dalzelldean@gmail.com>
2021-07-01 13:35:27 +0100

    Add items to cart

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -5,6 +5,9 @@ import { Card, Button, Container, Row, CardDeck } from 'react-bootstrap';
 import dmc from '../img/dmc.jpg';
 import bat from '../img/bat1.jpg';
 
+const PAGE_ITEMS = 'items';
+const PAGE_CART = 'cart';
+
 const Cars = () => {
   const styles = {
     fontFamily: 'sans-serif',
@@ -19,11 +22,90 @@ const Cars = () => {
   };
   const image1 =
     'https://images.unsplash.com/photo-1498092651296-641e88c3b057?auto=format&fit=crop&w=1778&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D';
+
+  const [cart, setCart] = useState([]);
+  const [page, setPage] = useState('items');
+
+  const [items] = useState([
+    {
+      name: 'DMC Delorean',
+      cost: '£50,000',
+      image: { dmc },
+      url: '/delorean',
+    },
+    {
+      name: 'Batmobile',
+      cost: '£500,000',
+      image: { bat },
+      url: '/bat',
+    },
+  ]);
+
+  const addToCart = (item) => {
+    setCart([...cart, item]);
+  };
+
+  const navigateTo = (nextPage) => {
+    setPage(nextPage);
+  };
+
+  const renderItems = () => (
+    <>
+      <h1>Hello</h1>
+      {items.map((item, index) => (
+        <div class="col">
+          <div class="card" key={index}>
+            <img src={dmc} class="card-img-top" alt="dmc" />
+            <div class="card-body">
+              <h5 class="card-title">{item.name}</h5>
+              <p class="card-text">{item.cost}</p>
+              <Link to={item.url}>
+                <button type="button" class="btn btn-primary btn-lg">
+                  More Details
+                </button>
+              </Link>
+              <button
+                onClick={() => addToCart(item)}
+                type="button"
+                class="btn btn-secondary btn-lg"
+              >
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+      ;
+    </>
+  );
+
+  const renderCart = () => (
+    <>
+      <h1>Cart</h1>;
+      {items.map((item, index) => (
+        <div class="col">
+          <div class="card" key={index}>
+            <img src={dmc} class="card-img-top" alt="dmc" />
+            <div class="card-body">
+              <h5 class="card-title">{item.name}</h5>
+              <p class="card-text">{item.cost}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+      ;
+    </>
+  );
+
   return (
     <div>
       <Parallax bgImage={image1} strength={500}>
         <div style={{ height: 500 }}>
-          <div style={insideStyles}></div>
+          <div style={insideStyles}>
+            <button onClick={() => navigateTo(PAGE_CART)}>
+              Go to Cart ({cart.length})
+            </button>
+          </div>
         </div>
       </Parallax>{' '}
       <div className="menu">
@@ -49,7 +131,7 @@ const Cars = () => {
         </ul>
       </div>
       <div className="gallery">
-        <div class="row row-cols-1 row-cols-md-4 g-4">
+        {/* <div class="row row-cols-1 row-cols-md-4 g-4">
           <div class="col">
             <div class="card">
               <img src={dmc} class="card-img-top" alt="dmc" />
@@ -130,6 +212,35 @@ const Cars = () => {
               </div>
             </div>
           </div>
+        </div> */}
+        <div class="row row-cols-1 row-cols-md-4 g-4">
+          {page === PAGE_ITEMS && renderItems()}
+          {page === PAGE_CART && renderCart()}
+          <>
+            {/* {items.map((item, index) => (
+                <div class="col">
+                  <div class="card" key={index}>
+                    <img src={dmc} class="card-img-top" alt="dmc" />
+                    <div class="card-body">
+                      <h5 class="card-title">{item.name}</h5>
+                      <p class="card-text">{item.cost}</p>
+                      <Link to={item.url}>
+                        <button type="button" class="btn btn-primary btn-lg">
+                          More Details
+                        </button>
+                      </Link>
+                      <button
+                        onClick={() => addToCart(item)}
+                        type="button"
+                        class="btn btn-secondary btn-lg"
+                      >
+                        Add to Cart
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))} */}
+          </>
         </div>
       </div>
     </div>
